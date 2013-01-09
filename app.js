195219233acf3c9f69b19446c907c65d86694ec1
Craig <ch.cr.co@gmail.com>
2013-01-09 15:23:21 -0500

    added status endpoint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  , api = require('./routes/twitter')
   , http = require('http')
   , path = require('path');
 
@@ -31,7 +31,7 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/statuses/show/:id', api.statusShow);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
---
+++ routes/twitter.js
@@ -0,0 +1,23 @@
+var twitter = require('ntwitter'),
+    util = require('util')
+
+function getTwitter() {
+    return new twitter({
+        consumer_key: 'ZZd1NQds3FNi83Df3K0fA',
+        consumer_secret: 'rLTJLbpIGA2dR6R6Hsod027t7jOMrgvW474d4x55Zc',
+        access_token_key: '100306546-OPrEPvY4667lkuDxyywx5LfxdCbemqpjzfzRq6JC',
+        access_token_secret: 'SZEguTBB38mzRzRj2SYFbJDJoK5dpSKZ0FBxr0KW8'
+    });
+}
+
+exports.statusShow = function(req, res, next) {
+    
+    var twitHandle = getTwitter();
+    
+    twitHandle.get('/statuses/show/'+req.params.id+'.json', {include_entities:true}, function(err, data) {
+        if(err) return next(err);
+        
+        res.send(data);
+    });
+    
+}
